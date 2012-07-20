41d732a5d0b524df7bc0b468876c6b3448890e05
Steve Geluso <stevegeluso@gmail.com>
2012-07-19 17:34:41 -0700

    removing more references to lobby2.js

--- pomme/rebuild.sh
+++ pomme/rebuild.sh
@@ -8,11 +8,11 @@ cd /Library/WebServer/Documents/git/Pomme/pomme/js/
 # PRODUCTION
 # cat jpegcam.js webcam.js api.js preload.js audio.js auth.js profile.js chat.js countdown.js game2.js > ~pomme/docs/js/pomme3.js
 cat jquery.wiggle.js jpegcam.js webcam.js api_game.js preload.js audio.js auth.js share.js profile.js chat.js countdown.js discard.js tooltip.js game_live.js > /Library/WebServer/Documents/git/Pomme/httpdocs/js/pomme_game.js
-cat api.js preload.js audio.js auth.js chat.js lobby.js > /Library/WebServer/Documents/git/Pomme/httpdocs/js/lobby2.js
+cat api.js preload.js audio.js auth.js chat.js lobby.js > /Library/WebServer/Documents/git/Pomme/httpdocs/js/lobby.js
 
 # DEVELOPMENT
 cat jquery.wiggle.js jpegcam.js webcam.js api_sandbox.js preload.js audio.js auth.js share.js profile.js chat.js countdown.js discard.js tooltip.js game_sandbox.js > /Library/WebServer/Documents/git/Pomme/httpdocs/js/pomme_sandbox.js
-cat api.js preload.js audio.js auth.js chat.js lobby2.js > /Library/WebServer/Documents/git/Pomme/httpdocs/js/lobby3.js
+cat api.js preload.js audio.js auth.js chat.js lobby.js > /Library/WebServer/Documents/git/Pomme/httpdocs/js/lobby3.js
 cat api.js preload.js audio.js auth.js chat.js profile.js profile_main.js > /Library/WebServer/Documents/git/Pomme/httpdocs/js/profile2.js
 
 cd ..
