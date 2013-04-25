97a89056614be8452c66a2844692509876b9d812
FrozenCow <frozencow@gmail.com>
2013-04-26 01:47:35 +0200

    Added login mechanism

--- node/app.js
+++ node/app.js
@@ -21,7 +21,7 @@ app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
   app.use(express.cookieParser('your secret here'));
-  app.use(express.session());
+  app.use(express.session({key:'sid'}));
 app.use(app.router);
   app.use(require('stylus').middleware(__dirname + '/public'));
 app.use(express.static(path.join(__dirname, 'public')));
