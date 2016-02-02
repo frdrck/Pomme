9c766acc653fe731927b1ab248de99e8e1e1b5a1
Steve Geluso <stevegeluso@gmail.com>
2016-02-01 18:23:20 -0800

    updating server config to new temporary server

--- pomme/config.py
+++ pomme/config.py
@@ -1,16 +1,16 @@
 # Server settings.
-SERVER_HOST = "pomme.us"
+SERVER_HOST = "heavyfeathered.com"
 SERVER_PORT = 32123
 FRUIT_URI_BASE	= "%s/img/fruit/" % (SERVER_HOST)
 
 # Database settings
-MYSQL_HOST = "pomme.us"
-MYSQL_USER = "pommeus"
+MYSQL_HOST = "mysql.heavyfeathered.com"
+MYSQL_USER = "asdfus"
 MYSQL_PASSWORD = "frederick"
 MYSQL_DATABASE = "asdfus"
 
 # Image path settings
-BASE_PATH	= "/var/www/vhosts/pomme.us/"
-PLAYER_CARDS	= "httpdocs/img/player/"
-MAIN_CARDS = "httpdocs/img/main/"
+BASE_PATH	= "/home/moonmayor/heavyfeathered.com/"
+PLAYER_CARDS	= "img/player/"
+MAIN_CARDS = "img/main/"
 
